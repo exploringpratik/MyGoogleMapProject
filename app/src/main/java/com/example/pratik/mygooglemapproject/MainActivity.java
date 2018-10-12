96b8a5e5db2e688b0277c87e27fe02d035fd84e4
Pratik Gautam <exploringpratik@gmail.com>
2018-10-12 21:22:47 +0545

    Marker added

--- app/src/main/java/com/example/pratik/mygooglemapproject/MainActivity.java
+++ app/src/main/java/com/example/pratik/mygooglemapproject/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -139,6 +140,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void afterPermission(){
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
         Toast.makeText(MainActivity.this,"It worked",Toast.LENGTH_LONG).show();
     }
 }
